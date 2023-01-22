7125ceb21b33731a3ee43cc766a61225fa78246a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-22 17:14:15 +0000

    quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -32,14 +32,16 @@ size_t partition(int *array, size_t lb, size_t ub, size_t size)
 		}
 		i++;
 	}
-	/*if (array[j] != array[piv])*/
+	/**
+	 * if (array[j] != array[piv])
+	 * if (j != piv)
+	 * {
+	 */
+	temp = array[j];
+	array[j] = array[piv];
+	array[piv] = temp;
 	if (j != piv)
-	{
-		temp = array[j];
-		array[j] = array[piv];
-		array[piv] = temp;
 		print_array(array, size);
-	}
 	return (j);
 }
 
